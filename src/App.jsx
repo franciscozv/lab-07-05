663e58dda35bceee5e4abf58b58e9d347d406486
Francisco Zambrano Valdés <102869648+franciscozv@users.noreply.github.com>
2025-05-14 12:53:43 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ const App = () => {
   return (
     <>
     <h1>Hola soy Francisco Zambrano Valdés</h1>
+      <p>Soy estudiante de Ingenieria De Ejecución en Computación e Informatica</p>
+      <p>Tengo conocimiento en el desarrollo de Apliaciones Web en las Tecnologias de ReactJs Express, Node. Base de datos relaciones y no relacionales (MongoDB, postgresql, Mysql) </p>
     
     </>
     );
